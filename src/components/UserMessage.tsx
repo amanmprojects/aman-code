db12e146710bab662e345c4190324a45f8aab01e
Aman Mehtar <amanmstudies@gmail.com>
2026-04-04 00:49:21 +0530

    Adjust space

--- src/components/UserMessage.tsx
+++ src/components/UserMessage.tsx
@@ -10,7 +10,7 @@ export default function UserMessage({ msg }: { msg: any }) {
     return (
         <Box key={msg.id} flexDirection='row' borderStyle='round' borderDimColor={true} borderLeft={false} borderRight={false}>
             <Text color="green" bold>
-                {'❯ '}
+                {' ❯ '}
             </Text>
             <Text>{msg.parts.filter((p: Part) => p.type === 'text').map((p: Part) => p.text).join('')}</Text>
         </Box>
